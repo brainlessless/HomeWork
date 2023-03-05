46f97a85223054a4fcbf3220dbe88c8f68f56182
Vitaly <109906595+brainlessless@users.noreply.github.com>
2023-03-05 19:43:53 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        //
-        //      System.out.print("Distance between cityA and cityB : ");
-        //      int distance = scanner.nextInt();
-        //      System.out.print("Time to get there in hours : ");
-        //      int time = scanner.nextInt();
-        //      int speed = distance * time;
-        //
-        //      System.out.print("You need to go at speed " + speed + "Km/h");
 
+    //   System.out.println("Task 1");
+
+    //   Scanner scanner = new Scanner(System.in);
+    //   System.out.println("Enter your name : ");
+    //   var name = scanner.nextLine();
+    //   System.out.println("Hello, " + name + "!");
+
+
+    //  System.out.println("Task 2");
+    //  Scanner scanner = new Scanner(System.in);
+
+    //  System.out.println("Enter your birth date : ");
+    //  int date = scanner.nextInt();
+    //  int birthdate = 2023 - date;
+    //  System.out.println("Your age is " + birthdate);
+
+    //   System.out.println("Task 3");
+    //   Scanner scanner = new Scanner(System.in);
+
+    //   System.out.print("Enter the length of the side of square : ");
+    //   int length = scanner.nextInt();
+    //   int perimeter = length * 4;
+    //   System.out.println("Perimeter of square is : " + perimeter);
+
+        //  System.out.println("Task 4");
+        //  Scanner scanner = new Scanner(System.in);
+
+        //  System.out.print("Enter the raduis of the circle : ");
+        //  double radius = scanner.nextDouble();
+        //  double perimeter = Math.PI * (radius * radius);
+        //  System.out.print("Perimeter of square is : " + perimeter);
+
+
+
+    //    System.out.println("Task 5");
+    //
+    //    Scanner scanner = new Scanner(System.in);
+    //    System.out.print("Distance between cityA and cityB : ");
+    //    int distance = scanner.nextInt();
+    //    System.out.print("Time to get there in hours : ");
+    //    int time = scanner.nextInt();
+    //    int speed = distance * time;
+    //    System.out.print("You need to go at speed " + speed + "Km/h");
+
+
+//     System.out.println("Task 6")";
+//
+//     Scanner scanner = new Scanner(System.in);
 //     System.out.print("Enter the amount in EUR : ");
 //     double amount = scanner.nextDouble();
 //     double currency = 1.06F;
@@ -20,27 +59,28 @@ public class Main {
 //     System.out.print(amount + "EUR = " + dollar + " USD");
 
 
- //    System.out.println("Enter flash drive size : ");
- //    double driveSize = scanner.nextDouble();
- //    double fileSize = 0.82F;
- //    double numberOfFiles = driveSize / fileSize;
+        //    System.out.println("Task 7");
 
- //    System.out.println("Numer of files : " + numberOfFiles);
-
-        System.out.print("Введите сумму денег : ");
-        int money = scanner.nextInt();
-        int chocolate = 45;
-        int amountOfChocolate = money / chocolate;
-        int shortCash = money - (amountOfChocolate * chocolate);
-
-        System.out.print("Количество шоколадок: " + amountOfChocolate + ", ваша сдача: " + shortCash);
+        //    Scanner scanner = new Scanner(System.in);
+        //    System.out.println("Enter flash drive size : ");
+        //    double driveSize = scanner.nextDouble();
+        //    double fileSize = 0.82F;
+        //    double numberOfFiles = driveSize / fileSize;
+        //    System.out.println("Numer of files : " + numberOfFiles);
 
 
-    }
-
-
-
-
-
+        //   System.out.println("Task 8");
+        //   Scanner scanner = new Scanner(System.in);
+        //   System.out.print("Введите сумму денег : ");
+        //   int money = scanner.nextInt();
+        //   int chocolate = 45;
+        //   int amountOfChocolate = money / chocolate;
+        //   int shortCash = money - (amountOfChocolate * chocolate);
+        //   System.out.print("Количество шоколадок: " + amountOfChocolate + ", ваша сдача: " + shortCash);
 
     }
+}
+
+
+
+
